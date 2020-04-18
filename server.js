92ca1a21e07079f14fdaa3e71e7404d278a5c42a
Omar Halbouni <omar.halbouni@accuv.com>
2020-04-18 04:55:03 -0400

    testing heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { Server } from 'boardgame.io/server'
 
 const PORT = process.env.PORT || 8000
 const server = Server({ games: [LegendOfAndor] })
+
 server.run(PORT, () => {
   console.log(`Serving at: http://localhost:${PORT}/`)
 })
