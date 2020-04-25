af74d38c0358971ad1e60dd7199f9b8622b88b77
Omar Halbouni <omarhalbouni@gmail.com>
2020-04-25 19:57:04 -0400

    Simplify server setup

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 import LegendOfAndor from './src/game'
 import { Server } from 'boardgame.io/server'
 import path from 'path'
-import Koa from 'koa'
 import serve from 'koa-static'
-import mount from 'koa-mount'
 
 const PORT = process.env.PORT || 8000
 const server = Server({ games: [LegendOfAndor] })
 
-const FRONTEND_APP_BUILD_PATH = path.resolve(__dirname, './build')
-const static_pages = new Koa()
-static_pages.use(serve(FRONTEND_APP_BUILD_PATH)) //serve the build directory
-server.app.use(mount('/', static_pages)) //serve the build directory
+const root = path.resolve(__dirname, './build')
+server.app.use(serve(root))
 
 server.run(PORT, () => {
-  server.app.use(
-    async (ctx, next) => await serve(FRONTEND_APP_BUILD_PATH)(Object.assign(ctx, { path: 'index.html' }), next)
-  )
+  server.app.use(async (ctx, next) => await serve(root)(Object.assign(ctx, { path: 'index.html' }), next))
 })
